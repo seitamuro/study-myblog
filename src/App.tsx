c28971cea9e0e3ff44ccd4d133cc6e15598dfd8c
seitamuro <seitamuro0210@gmail.com>
2024-02-06 23:28:33 +0900

    GraphQLを通じてデータをフェッチできるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Amplify, ResourcesConfig } from "aws-amplify";
+import { generateClient } from "@aws-amplify/api";
+import config from "./aws-exports.js";
+import { useEffect, useState } from "react";
+
+import { listMyQuery } from "./graphql/query";
+
+Amplify.configure(config as ResourcesConfig);
+const client = generateClient(config.API.GraphQL as any);
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetchQuery() as any
+      if (res.data) {
+        console.log(res.data.listMyCustomTypes.items)
+        setData(res.data.listMyCustomTypes.items)
+      }
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!data ? "Loading..." : data.map((item: any) => <p>{`${item.title}: ${item.content}`}</p>)}
+    </>
   );
+}
+
+async function fetchQuery() {
+  try {
+    const data = await client.graphql({
+      query: listMyQuery
+    })
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+  return []
 }
 
 export default App;
